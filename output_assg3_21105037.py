a843a91f1a32dd38e5761159131edae5bf7decc2
Chir4g27 <96478040+Chir4g27@users.noreply.github.com>
2022-02-14 22:53:15 +0530

    Add files via upload

---
+++ output_assg3_21105037.py
@@ -0,0 +1,92 @@
+Python 3.9.10 (v3.9.10:f2f3f53782, Jan 13 2022, 17:02:14) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============== RESTART: /Users/chirag/Desktop/21105037_assg3.py ===============
+solution of Question 1 :
+
+Enter the string: my my name is is chirag
+The occurence(s) of:
+	"my	" is/are 2
+	"name	" is/are 1
+	"is	" is/are 2
+	"chirag	" is/are 1
+
+solution of Question 2 is:
+
+Day - 27
+Month - 05
+Year - 2003
+Next Date is: 28/5/2003
+
+solution of Question 3 is:
+
+Enter the list: 1,2,3
+Enter the list of integers: 1,2,3
+Output: [(1, 1), (2, 4), (3, 9)]
+
+solution of Question 4 is:
+
+Enter the grade point of the student: 10
+Your Grade is 'A+' and Outstanding Performance
+
+solution of Question 5 is:
+
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+The solution of Question 6 is:
+
+Enter the name of the student: chirag
+Enter the SID of chirag: 21105037
+
+You have entered 1 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: aryan
+Enter the SID of aryan: 21105040
+
+You have entered 2 value(s) till now
+Do you want to enter more data? y
+Enter the name of the student: soumil
+Enter the SID of soumil: 21105005
+
+You have entered 3 value(s) till now
+Do you want to enter more data? n
+
+a. Student Details:
+The SID of chirag\ is 21105037
+The SID of aryan\ is 21105040
+The SID of soumil\ is 21105005
+
+b. Student Details (sorted with respect to names):
+The SID of aryan is 21105040
+The SID of chirag is 21105037
+The SID of soumil is 21105005
+
+c. Student Details (sorted with respect to SIDs):
+The SID of soumil is 21105005
+The SID of chirag is 21105037
+The SID of aryan is 21105040
+
+d. Enter the SID of the student: 21105037
+The name of student whose SID is 21105037 is chirag
+
+The solution of Question 7 is:
+
+Enter the no. of terms of the Fibonacci sequence: 7
+The Fibonacci sequence is as follows:
+0 1 1 2 3 5 8 
+The average of the terms of Fibonacci sequence upto 7 terms is 2.86
+
+The solution of Question 8 is:
+
+a. Set of all elements that are in Set1 and Set2 but not both: {1, 3, 5, 6, 8}
+b. Set of all elements that are in only one of the three sets Set1, Set2 and Set3: {17, 3, 6, 8, 9, 13}
+c. Set of elements that are in exactly two of the sets Set1, Set2 and Set3: {1, 2, 4, 5}
+d. Set of all integers in the range 1 to 10 that are not in Set1: {6, 7, 8, 9, 10}
+e. Set of all integers in the range 1 to 10 that are not in Set1, Set2 and Set3: {10, 7}
+>>> 
